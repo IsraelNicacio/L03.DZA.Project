f2d59e1cb3f5de953a28fad02244e4a5b7994f30
Israel Nicacio <israel.nicacio@outlook.com>
2025-04-07 20:36:19 -0300

    Criando dependencias, configuracoes e registrando o tenant na classe program

---
+++ src/Cadastro/DZA.Cadastro.Api/Configurations/ApiConfiguration.cs
@@ -0,0 +1,35 @@
+﻿namespace DZA.Cadastro.Api.Configurations;
+
+public static class ApiConfiguration
+{
+    const string AllowAllPolicy = "AllowAll";
+
+    public static void AddApiConfiguration(this IServiceCollection services)
+    {
+        services.AddControllers();
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+
+        services.AddCors(options =>
+        {
+            options.AddPolicy(AllowAllPolicy
+                , builder => 
+                builder.AllowAnyOrigin()
+                .AllowAnyHeader()
+                .AllowAnyMethod());
+        });
+    }
+
+    public static void UseApiConfiguration(this WebApplication app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment() || env.IsEnvironment("Stage") || env.IsEnvironment("Docker"))
+            app.UseDeveloperExceptionPage();
+
+        if (app.Configuration["USE_HTTPS_REDIRECTION"] == "true")
+            app.UseHttpsRedirection();
+
+        app.UseCors(AllowAllPolicy);
+        app.UseAuthorization();
+        app.MapControllers();
+    }
+}
---
+++ src/Cadastro/DZA.Cadastro.Api/Configurations/SwaggerConfiguration.cs
@@ -0,0 +1,13 @@
+﻿namespace DZA.Cadastro.Api.Configurations;
+
+public static class SwaggerConfiguration
+{
+    public static void UseSwaggerConfig(this WebApplication app)
+    {
+        if (app.Environment.IsDevelopment() || app.Environment.IsEnvironment("Stage") || app.Environment.IsEnvironment("Docker"))
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+    }
+}
