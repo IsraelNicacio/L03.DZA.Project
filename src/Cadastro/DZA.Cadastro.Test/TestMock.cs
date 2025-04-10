2af2c8648e34f49b0ef756c89faa9cb9cf8821d5
Israel Nicacio <israel.nicacio@outlook.com>
2025-04-10 13:08:02 -0300

    Criando projeto de teste

---
+++ src/Cadastro/DZA.Cadastro.Test/TestMock.cs
@@ -0,0 +1,18 @@
+﻿namespace DZA.Cadastro.Test;
+
+
+public class TestMock
+{
+    [Fact]
+    public void Calculadora_Somar_RetornarValorSoma()
+    {
+        // Arrange
+
+
+        // Act
+        var resultado = (2 + 2);
+
+        // Assert
+        Assert.AreEqual(4, resultado);
+    }
+}
